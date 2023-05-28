a1354cf3e4134e785520f20dfe9a99da9a9020e3
sim1000 <sim19956@gmail.com>
2023-05-28 18:26:56 +0300

    Add app version

--- src/Converter/Main.java
+++ src/Converter/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Converter for base converting");
+        System.out.println("Version 1.0");
     }
 }
