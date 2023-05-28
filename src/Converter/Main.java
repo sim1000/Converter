cb4d3f374f43b7deaa5f52c49bbf14b7f17c53dc
sim1000 <sim19956@gmail.com>
2023-05-28 18:25:20 +0300

    Add converter miles

--- src/Converter/Main.java
+++ src/Converter/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("My git app.");
+        double miles = 15;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+    }
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
     }
 }
